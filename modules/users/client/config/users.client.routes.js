b1445fad596a7a81f19d481531d900b3f2b84006
Vic Wang <vicwang95@gmail.com>
2015-11-20 20:20:23 -0500

    fix view bugs, add basic user component

---
+++ modules/users/client/config/users.client.routes.js
@@ -0,0 +1,52 @@
+'use strict';
+
+// Setting up route
+angular.module('users').config(['$stateProvider',
+  function ($stateProvider) {
+    // Users state routing
+    $stateProvider
+      .state('settings', {
+        abstract: true,
+        url: '/settings',
+        templateUrl: 'modules/users/client/views/settings/settings.client.view.html',
+        data: {
+          roles: ['user', 'admin']
+        }
+      })
+      .state('settings.profile', {
+        url: '/profile',
+        templateUrl: 'modules/users/client/views/settings/edit-profile.client.view.html'
+      })
+      .state('settings.password', {
+        url: '/password',
+        templateUrl: 'modules/users/client/views/settings/change-password.client.view.html'
+      })
+      .state('settings.accounts', {
+        url: '/accounts',
+        templateUrl: 'modules/users/client/views/settings/manage-social-accounts.client.view.html'
+      })
+      .state('settings.picture', {
+        url: '/picture',
+        templateUrl: 'modules/users/client/views/settings/change-profile-picture.client.view.html'
+      })
+      .state('authentication', {
+        abstract: true,
+        url: '/authentication',
+        templateUrl: 'modules/users/client/views/authentication/authentication.client.view.html'
+      })
+      .state('authentication.signup', {
+        url: '/signup',
+        templateUrl: 'modules/users/client/views/authentication/signup.client.view.html'
+      })
+      .state('authentication.signin', {
+        url: '/signin?err',
+        templateUrl: 'modules/users/client/views/authentication/signin.client.view.html'
+      })
+      .state('password', {
+        abstract: true,
+        url: '/password',
+        template: '<ui-view/>'
+      });
+
+  }
+]);
