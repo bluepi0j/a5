f51c5a3b88c24b78f415d7ae317fc6c77c7ad150
Vic Wang <vicwang95@gmail.com>
2015-11-24 18:15:05 -0500

    update assignment title

--- config/env/default.js
+++ config/env/default.js
@@ -5,7 +5,7 @@
 
 module.exports = {
     app: {
-        title: 'a4',
+        title: 'a5',
         description: 'Full-Stack JavaScript with MongoDB, Express, AngularJS, and Node.js',
         keywords: 'mongodb, express, angularjs, node.js, mongoose, passport',
         //googleAnalyticsTrackingID: process.env.GOOGLE_ANALYTICS_TRACKING_ID || 'GOOGLE_ANALYTICS_TRACKING_ID'
