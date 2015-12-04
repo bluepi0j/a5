ed851d4a3db64d7ad196c891ff5249a1ed4331b2
KENAN DENG <dknyxh@gmail.com>
2015-12-04 16:53:53 -0500

    Fix search

--- modules/sketchpad/server/controllers/sketchpad.search.server.controller.js
+++ modules/sketchpad/server/controllers/sketchpad.search.server.controller.js
@@ -9,14 +9,19 @@ var path = require('path'),
     mongoose = require('mongoose'),
     config = require(path.resolve('./config/config')),
     Sketchpad = mongoose.model('Sketchpad'),
-    Uer = mongoose.model('User');
+    User = mongoose.model('User');
 
 
 exports.searchTitle = function(req,res){
-    Sketchpad.find({ title: { $regex: '/^'+req.params.titleString+'.*/' } }).sort('-created').exec(function(err, sketch) {
+    Sketchpad.find({ title: { $regex:  new RegExp('^'+req.params.titleString) } }).sort('-created').exec(function(err, sketch) {
         if (err) {
             return res.status(400).send({
                 message: errorHandler.getErrorMessage(err)
+            });
+        }
+        else if(sketch == null){
+            return res.status(400).send({
+                message: 'No matches found'
             });
         }
         res.json(sketch);
@@ -25,25 +30,33 @@ exports.searchTitle = function(req,res){
 }
 
 exports.searchUser = function(req,res){
-    User.findOne({ displayName: req.params.userString }).sort('-created').exec(function(err, user) {
+    User.find({ userName: req.params.userString }).sort('-created').exec(function(err, user) {
         if (err) {
             return res.status(400).send({
                 message: errorHandler.getErrorMessage(err)
             });
         }
-        var i = 0;
-        var list = [];
-        for (i = 0;i<user.length;i++){
-             Sketchpad.find({authorID:user.id},function(err,sketch){
-                if(err){
-                    return res.status(400).send({
-                        message: errorHandler.getErrorMessage(err)
-                    });
-                }
-                list.push(sketch);
-
-             })
+        else if(user == null){
+            return res.status(400).send({
+                message: 'No matches found'
+            });
         }
+        else{        
+            var i = 0;
+            var list = [];
+            for (i = 0;i<user.length;i++){
+                Sketchpad.find({authorID:user.id},function(err,sketch){
+                    if(err){
+                        return res.status(400).send({
+                            message: errorHandler.getErrorMessage(err)
+                        });
+                    }
+                    list.push(sketch);
+
+                })
+            }
+        }
+
        
         res.json(list);
 
