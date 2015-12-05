92d4e27b5167d90d1c8e1fbce68c52a75dae9188
Kenan Deng <dknyxh@gmail.com>
2015-12-04 20:20:19 -0500

    sketch search UI start

--- modules/sketchpad/client/config/sketch.client.routes.js
+++ modules/sketchpad/client/config/sketch.client.routes.js
@@ -2,20 +2,25 @@
 
 // Setting up route
 angular.module('sketch').config(['$stateProvider',
-  function ($stateProvider) {
-    // Users state routing
-    $stateProvider
-      .state('sketch-create', {
-        url: '/sketch/create',
-        templateUrl: 'modules/sketchpad/client/views/sketch-new.client.view.html',
-        data: {
-          roles: ['user', 'admin']
-        }
-      })
-      .state('sketch-comment', {
-        url: '/sketch/comment/:sketckId',
-        templateUrl: 'modules/sketchpad/client/views/sketch-comment.client.view.html'
-      });
+    function ($stateProvider) {
+        // Users state routing
+        $stateProvider
+            .state('sketch-create', {
+                url: '/sketch/create',
+                templateUrl: 'modules/sketchpad/client/views/sketch-new.client.view.html',
+                data: {
+                    roles: ['user', 'admin']
+                }
+            })
+            .state('sketch-comment', {
+                url: '/sketch/comment/:sketckId',
+                templateUrl: 'modules/sketchpad/client/views/sketch-comment.client.view.html'
+            })
+            .state('sketch-search', {
+                url: '/sketch/search',
+                templateUrl: 'modules/sketchpad/client/views/sketch-search.client.view.html'
 
-  }
+            });
+
+    }
 ]);
